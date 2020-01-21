60d4b7d55945fb61441e1955c92b77f79d260c48
Falko Schumann <falko.schumann@muspellheim.de>
2020-01-21 23:14:02 +0100

    JSON weiterentwickelt

--- src/main/kotlin/json/Json.kt
+++ src/main/kotlin/json/Json.kt
@@ -1,10 +1,41 @@
 package de.muspellheim.userstories.json
 
 sealed class JsonValue
-class JsonString : JsonValue()
-class JsonNumber : JsonValue()
-class JsonObject : JsonValue()
-class JsonArray : JsonValue()
-object JsonTrue : JsonValue()
-object JsonFalse : JsonValue()
-object JsonNull : JsonValue()
+
+data class JsonString(val value: String) : JsonValue() {
+    override fun toString(): String {
+        return value
+    }
+}
+
+data class JsonNumber(val value: Number) : JsonValue() {
+    override fun toString(): String {
+        return value.toString()
+    }
+}
+
+data class JsonObject(val members: Map<String, JsonValue>) : JsonValue() {
+    operator fun get(key: String): JsonValue = members[key] ?: error("member does not exist: $key")
+}
+
+data class JsonArray(val values: List<JsonValue>) : JsonValue() {
+    operator fun get(index: Int): JsonValue = values[index]
+}
+
+object JsonTrue : JsonValue() {
+    override fun toString(): String {
+        return "true"
+    }
+}
+
+object JsonFalse : JsonValue() {
+    override fun toString(): String {
+        return "false"
+    }
+}
+
+object JsonNull : JsonValue() {
+    override fun toString(): String {
+        return "null"
+    }
+}
